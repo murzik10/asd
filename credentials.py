646ca6fb121bc3e2642f2a05fb4dedab4aa73620
mac <mac@MacBook-Pro-mac.local>
2021-11-22 18:18:12 +0600

    asd

---
+++ credentials.py
@@ -0,0 +1,9 @@
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = 'https://www.alphavantage.co/query'
+NEWS_ENDPOINT = 'https://newsapi.org/v2/everything'
+
+STOCK_API_KEY = 'R1KOXXSMTFCN74W8'
+NEWS_API_KEY = '1de75ed7bd7b415384b99e85b4efb3e3'
+BOT_TOKEN = '2124399354:AAHOUBKsyUbrG2JQVvmVuushYnQpQOscJhM'
